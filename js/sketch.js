3f5881cde4912d119159ff80f848378e5265f265
Linde0404 <dielangeemail@gmail.com>
2019-03-15 18:10:53 +0100

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,19 +1,25 @@
 let clrButton;
 
 function setup(){
+  // Create Canvas
   let canvas = createCanvas(280, 280);
   canvas.parent("canvas");
+  
+  // Leinwand wir mit weiß gefüllt
   background(255);
-  clrButton = document.getElementById("clrButton");
+  
+  // Setzt die Leinwand bei Klick auf den Löschen-Knopf zurück
+  document.getElementById("clrButton")addEventListener("click", (ev)=>{
+    background(255);
+  });
 }
 
 function draw(){
+  // Schwarzer Pinsel
   stroke(0);
   strokeWeight(4);
   if(mouseIsPressed){
+    // Wenn die Maus gerückt wird, dann zeichne eine Linie
     line(pmouseX, pmouseY, mouseX, mouseY);
   }
-  clrButton.mousePressed(()=>{
-    background(255);
-  });
 }
