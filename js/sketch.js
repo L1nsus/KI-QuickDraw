e127215fef1a4d680c574eced5923f8667a8e6e1
Linde0404 <dielangeemail@gmail.com>
2019-03-15 17:36:25 +0100

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,13 @@
 function setup(){
   let canvas = createCanvas(280, 280);
   canvas.parent("canvas");
-  background("white");
+  background(255);//"white");
+}
+
+function draw(){
+  stroke(0);
+  strokeWeight(4);
+  if(mouseIsPressed){
+    line(pmouseX, pmouseY, mouseX, mouseY);
+  }
 }
