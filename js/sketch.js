08c8910d2b8a69f7ae7f83f67a17512d885ffaae
Linde0404 <dielangeemail@gmail.com>
2019-03-15 16:32:54 +0100

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,10 +1,12 @@
 function onloaded(ev) {
   const canvasNode = document.getElementById('canvas');
   const canvas = canvasNode.getContext('2d');
-  
+  clearCanvas();
+}
+
+function clearCanvas(){
   canvas.fillStyle = 'rgb(255, 255, 255)';
   canvas.fillRect(0, 0, 280, 280);
-  
 }
 
 document.addEventListener("DOMContentLoaded", onloaded);
