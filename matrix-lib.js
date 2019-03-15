6c65656be458e295202e43b616d63e24d33b5160
Linde0404 <dielangeemail@gmail.com>
2019-03-15 09:08:28 +0100

    Update matrix-lib.js

--- matrix-lib.js
+++ matrix-lib.js
@@ -1,6 +1,6 @@
 // Authors: Linus Nadler, Dominik Dreiheller
 // License: Apache 2.0 http://www.apache.org/licenses/LICENSE-2.0.html
-// Contents: Librarry fort Matrix Math
+// Contents: Library fort Matrix Math
 
 class Matrix{
   constructor(n, k){
