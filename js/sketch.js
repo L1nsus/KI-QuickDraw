a305eca008958605e5aca7a019085ad391955080
Linde0404 <dielangeemail@gmail.com>
2019-03-15 16:51:19 +0100

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -8,7 +8,7 @@ function onloaded(ev) {
 
 
   function clearCanvas(){
-    canvas.fillStyle = "51";//'rgb(255, 255, 255)';
+    canvas.fillStyle = "0";//'rgb(255, 255, 255)';
     canvas.fillRect(0, 0, width, height);
   }
 }
