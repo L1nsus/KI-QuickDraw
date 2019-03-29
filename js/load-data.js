b7274f3bf0830b762f30f8849f526a5055791edf
Linus N. Nadler <dielangeemail@gmail.com>
2019-03-29 17:40:09 +0100

    Update load-data.js

--- js/load-data.js
+++ js/load-data.js
@@ -1,46 +1,36 @@
-function loadDataFromFile(path){
-  fetch(path).then(
-    DATA => DATA.text()
-  ).then(
-    TEXT => {
-      console.log(TEXT);
-    }
-  ).catch(
-    ERR => {
-      console.error(ERR);
-      throw(ERR);
-    }
-  );
-}
-
 async function loadDataFromFile(URL){
 
-let response = await fetch(URL);
+  let response = await fetch(URL);
 
-const reader = response.body.getReader();
-const contentLength = +response.headers.get('Content-Length');
+  const reader = response.body.getReader();
+  const contentLength = +response.headers.get('Content-Length');
 
-let receivedLength = 0;
-let chunks = [];
-while(true) {
-  const {done, value} = await reader.read();
+  let receivedLength = 0;
+  let chunks = [];
+  while(true) {
+    const {done, value} = await reader.read();
 
-  if (done) {
-    break;
+    if (done) {
+      break;
+    }
+
+    chunks.push(value);
+    receivedLength += value.length;
+
+    //console.log(Math.round(100 * (receivedLength / contentLength)) + "% fertig ...");
+    updateModal(Math.round(100 * (receivedLength / contentLength)));
   }
 
-  chunks.push(value);
-  receivedLength += value.length;
-
-  console.log(Math.round(100 * (receivedLength / contentLength)) + "% fertig ...");
+  let chunksAll = new Uint8Array(receivedLength);
+  let position = 0;
+  for(let chunk of chunks) {
+    chunksAll.set(chunk, position);
+    position += chunk.length;
+  }
+  console.log(chunksAll);
+  return chunksAll;
 }
 
-let chunksAll = new Uint8Array(receivedLength);
-let position = 0;
-for(let chunk of chunks) {
-  chunksAll.set(chunk, position);
-  position += chunk.length;
-}
-console.log(chunksAll);
-return chunksAll;
+function updateModal(percentage){
+  document.getElemetById("progbar").style.width=percentage+"%";
 }
