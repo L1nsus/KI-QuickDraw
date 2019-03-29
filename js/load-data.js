7c0a22116303f273bf7f68ffda7a097b39ae6b9b
Linus N. Nadler <dielangeemail@gmail.com>
2019-03-29 16:34:45 +0100

    Update load-data.js

--- js/load-data.js
+++ js/load-data.js
@@ -1,1 +1,14 @@
-
+function loadDataFromFile(path){
+  fetch(path).then(
+    DATA => DATA.text()
+  ).then(
+    TEXT => {
+      console.log(TEXT);
+    }
+  ).catch(
+    ERR => {
+      console.error(ERR);
+      throw(ERR);
+    }
+  );
+}
