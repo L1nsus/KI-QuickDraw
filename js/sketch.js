26474072e5576087d8245fd728304a74778feb67
Linus N. Nadler <dielangeemail@gmail.com>
2019-04-03 18:47:21 +0200

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,4 +1,5 @@
 let clrButton;
+let appleDataArray, basketballsDataArray;//, lightbulbsDataArray, pizzasDataArray, swordsDataArray;
 
 function setup(){
   // Create Canvas
@@ -10,6 +11,9 @@ function setup(){
   
   // Setzt die Leinwand bei Klick auf den Löschen-Knopf zurück
   document.getElementById("clrButton").addEventListener("click", () => background(255));
+  
+  // Datensets nach 3 Sekunden laden
+  window.setTimeout(() => loadData(), 3000);
 }
 
 function draw(){
@@ -20,4 +24,25 @@ function draw(){
     // Wenn die Maus gerückt wird, dann zeichne eine Linie
     line(pmouseX, pmouseY, mouseX, mouseY);
   }
+}
+
+function loadData(){
+  loadDataFromFile("./data/apples10000.bin").then( DATA =>{
+    appleDataArray = DATA;
+    loadDataFromFile("./data/basketballs10000.bin").then( DATA => {
+      basketballsDataArray = DATA;
+      loadDataFromFile("./data/lightbulbs10000.bin").then( DATA => {
+        lightbulbsDataArray = DATA;
+        loadDataFromFile("./data/pizzas10000.bin").then( DATA => {
+          pizzasDataArray = DATA;
+          loadDataFromFile("./data/swords10000.bin").then( DATA => {
+            swordsDataArray = DATA;
+          });
+        });
+      });
+    });
+  }).catch(ERR => {
+    console.err(ERR);
+    console.log(this);
+  });
 }
