1efef11b484610af18fc63be9578840c419cb2ba
Linus N. Nadler <dielangeemail@gmail.com>
2019-04-03 20:13:26 +0200

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -42,6 +42,11 @@ function loadData(){
             pizzasDataArray = DATA;
             loadDataFromFile("./data/swords10000.bin").then( DATA => {
               swordsDataArray = DATA;
+              document.getElementById("info-wrapper").style.display="block";
+              document.getElementById("info-text").innerHTML="<div class=\"w3-xxxlarge\">\
+                <span onclick=\"document.getElementById(\'info-wrapper\').style.display=\'none\'\"\
+                  class=\"w3-button w3-display-topright\"><i class=\"material-icons w3-xlarge\">cancel</i></span>\
+                Fertig <i class=\"material-icons w3-xxxlarge\">cloud_done</i></div>";
             });
           });
         });
@@ -54,10 +59,5 @@ function loadData(){
       throw(ERR);
       return null;
     });
-    document.getElementById("info-wrapper").style.display="block";
-    document.getElementById("info-text").innerHTML="<div class=\"w3-xxxlarge\">\
-      <span onclick=\"document.getElementById(\'info-wrapper\').style.display=\'none\'\"\
-        class=\"w3-button w3-display-topright\"><i class=\"material-icons w3-xlarge\">cancel</i></span>\
-      Fertig <i class=\"material-icons w3-xxxlarge\">cloud_done</i></div>";
   }, 3000);
 }
