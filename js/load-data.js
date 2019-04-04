0d6aad2ac3bfe48e79023a104eca1b8e7e28d2e3
Linus N. Nadler <dielangeemail@gmail.com>
2019-04-04 18:26:21 +0200

    Update load-data.js

--- js/load-data.js
+++ js/load-data.js
@@ -37,7 +37,7 @@ function updateModal(receivedLength, contentLength, URL){
   let len = contentLength.toString().length;
   document.getElementById("progbar-text").innerHTML="Gerade wird der Datensatz &#187;" +  URL.split("/").reverse()[0] + 
     "&#171; geladen. <i class=\"material-icons w3-display-topright\">save_alt</i><br><div class=\"w3-center\">\
-    <span class=\"num\">" + Math.round(perc) + "% fertig (" + nf(receivedLength, len) + 
-    "/" + contentLength + "</span>)</div>";
+    <span class=\"num monospace\">" + Math.round(perc) + "</span>% fertig (<span class=\"num monospace\">" + 
+    nf(receivedLength, len) + "</span>/<span class=\"num monospace\">" + contentLength + "</span>)</div>";
   document.getElementById("progbar").style.width=perc+"%";
 }
