3134efa3496955152acff50a5fbf2e4c4cd782e5
Linus N. Nadler <dielangeemail@gmail.com>
2019-04-04 19:09:19 +0200

    Update load-data.js

--- js/load-data.js
+++ js/load-data.js
@@ -3,6 +3,7 @@ async function loadDataFromFile(URL){
   let response = await fetch(URL);
   
   w3.show("#progbar-wrapper");
+  w3.addStyle("body", "overflow", "hidden");
 
   const reader = response.body.getReader();
   const contentLength = +response.headers.get('Content-Length');
@@ -29,6 +30,7 @@ async function loadDataFromFile(URL){
     position += chunk.length;
   }
   w3.hide("#progbar-wrapper");
+  w3.addStyle("body", "overflow", "initial");
   return await Promise.resolve(chunksAll);
 }
 
